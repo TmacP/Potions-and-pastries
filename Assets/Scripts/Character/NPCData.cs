747bd21a9b0d4ac174b24bcbac93021d276186e3
LindseyER <100873359+LindseyER@users.noreply.github.com>
2024-02-06 16:57:46 -0600

    NPCData script done

---
+++ Assets/Scripts/Character/NPCData.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ENPCArchetype
+{
+    Rogue,
+    Magician,
+    Ranger,
+    Fighter
+}
+
+public class NPCData : ScriptableObject
+{
+    public string Name;
+    public List<EItemTags> NPCLikes;
+    public List<EItemTags> NPCDislikes;
+    public Sprite image;
+}
+
+[Serializable]
+public class NPCCharacterData
+{
+
+    public NPCData Data;
+    public NPCCharacterData(NPCData inData)
+    {
+        Data = inData;
+    }
+}
