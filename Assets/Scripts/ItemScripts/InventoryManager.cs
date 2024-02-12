c3aa71e0f63b76a482affe8c114691af59195ed8
Conor <34079159+ConorDennison@users.noreply.github.com>
2024-02-11 18:47:58 -0600

    removed includes preventing build

--- Assets/Scripts/ItemScripts/InventoryManager.cs
+++ Assets/Scripts/ItemScripts/InventoryManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
-using static UnityEditor.Progress;
 
 public class InventoryManager : MonoBehaviour
 {
