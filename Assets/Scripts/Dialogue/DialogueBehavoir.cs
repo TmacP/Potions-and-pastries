7f69a79436f3cb8cc1e387585033d5f9e0b79831
Conor <34079159+ConorDennison@users.noreply.github.com>
2024-02-19 09:00:17 -0600

    Screen Space Dialogue

--- Assets/Scripts/Dialogue/DialogueBehavoir.cs
+++ Assets/Scripts/Dialogue/DialogueBehavoir.cs
@@ -10,6 +10,8 @@ public class DialogueBehavoir : MonoBehaviour
     [SerializeField] public NPCDialogueState State;
     [SerializeField] public GameObject DialogueUI;
     [SerializeField] public TextMeshProUGUI DialogueText;
+    [SerializeField] public float DialogueDisplayTime = 3.0f;
+    [SerializeField] public int DialogueDisplayCounter = 0;
 
     public void TryDialogue()
     {
@@ -21,6 +23,8 @@ public class DialogueBehavoir : MonoBehaviour
             {
                 DialogueUI.SetActive(true);
                 DialogueText.SetText(Dialogue.Dialogue);
+                DialogueDisplayCounter++;
+                Invoke("EndQuipDialogue", DialogueDisplayTime);
             }
             else
             {
@@ -31,6 +35,16 @@ public class DialogueBehavoir : MonoBehaviour
         else
         {
             Debug.Log("Null Dialogue Return");
+        }
+    }
+
+
+    public void EndQuipDialogue()
+    {
+        DialogueDisplayCounter--;
+        if(DialogueDisplayCounter <= 0)
+        {
+            DialogueUI.SetActive(false);
         }
     }
  
