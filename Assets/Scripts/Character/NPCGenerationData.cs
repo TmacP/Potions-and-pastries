8db9631db9cba294b68ffe49291b31cb9241baaf
AndyMandyCandy <andyanan124@gmail.com>
2024-04-02 20:31:36 -0600

    fixed some typos in NPCGenerationData

--- Assets/Scripts/Character/NPCGenerationData.cs
+++ Assets/Scripts/Character/NPCGenerationData.cs
@@ -8,10 +8,10 @@ public class NPCGenerationData : MonoBehaviour
     static public readonly Dictionary<ECharacterSpriteAssetSlots, List<string>> SpriteLibrary = new Dictionary<ECharacterSpriteAssetSlots, List<string>>()
     {
         { ECharacterSpriteAssetSlots.Hat, new List<string>() { "ConeHat", "LeatherHat", "None", "WizardHat", "HornedHat"} },
-        { ECharacterSpriteAssetSlots.Hair, new List<string>() { "ScruffyHair", "SpikeyHair", "None", "LongBang", "SlickedHair" } },
+        { ECharacterSpriteAssetSlots.Hair, new List<string>() { "ScruffyHair", "SpikeyHair", "None", "LongBangsHair", "SlickedHair" } },
         { ECharacterSpriteAssetSlots.Eye, new List<string>() { "NormalEye", "LashesEye", "SleepyEye", "XDEye"} },
         { ECharacterSpriteAssetSlots.Nose, new List<string>() { "SmallTriangleNose", "BigNose", "LongTriangleNose", "ButtonNose"} },
-        { ECharacterSpriteAssetSlots.Mouth, new List<string>() { "VMouth", "OpenSmile", "FlatMouth", "OMouth"} },
+        { ECharacterSpriteAssetSlots.Mouth, new List<string>() { "VMouth", "OpenSmileMouth", "FlatMouth", "OMouth"} },
         { ECharacterSpriteAssetSlots.Torso, new List<string>() { "BlankTorso", "GambesonTorso", "PuffyTorso", "ArmoredTorso"} },
         { ECharacterSpriteAssetSlots.Arm, new List<string>() { "BlankArm", "PaddedArm", "PuffyArm", "ArmoredArm"} },
         { ECharacterSpriteAssetSlots.Bottom, new List<string>() { "BlankPants", "TrouserPants", "JesterPants", "ArmoredPants"} },
