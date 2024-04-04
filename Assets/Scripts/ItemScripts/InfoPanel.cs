1336a1ca7c5fddf3dee6eb2b5730e52e7338dc17
Conor <34079159+ConorDennison@users.noreply.github.com>
2024-04-04 10:12:54 -0600

    clean up info panel

--- Assets/Scripts/ItemScripts/InfoPanel.cs
+++ Assets/Scripts/ItemScripts/InfoPanel.cs
@@ -11,8 +11,8 @@ public class InfoPanel : MonoBehaviour
     public TMP_Text InfoBoxDescription;
     public TMP_Text InfoBoxTags;
     public Image InfoBoxImage;
-    public Button UseItemButton;
-    public Button DestroyItemButton;
+    //public Button UseItemButton;
+    //public Button DestroyItemButton;
 
     public void SetInfo(string name, string description, HashSet<EItemTags> Tags, Sprite image)
     {
@@ -34,8 +34,8 @@ public class InfoPanel : MonoBehaviour
         InfoBoxTags.text = TagString;
         InfoBoxImage.sprite = image;
         InfoBoxImage.color = new Color(1, 1, 1);
-        UseItemButton.gameObject.SetActive(true);
-        DestroyItemButton.gameObject.SetActive(true);
+        //UseItemButton.gameObject.SetActive(true);
+        //DestroyItemButton.gameObject.SetActive(true);
        
     }
 
@@ -45,8 +45,8 @@ public class InfoPanel : MonoBehaviour
         InfoBoxName.text = "";
         InfoBoxDescription.text = "";
         InfoBoxImage.sprite = null;
-        UseItemButton.gameObject.SetActive(false);
-        DestroyItemButton.gameObject.SetActive(false);
+        //UseItemButton.gameObject.SetActive(false);
+        //DestroyItemButton.gameObject.SetActive(false);
         // Convert color values from 0-255 range to 0-1 range
         float r = 212f / 255f;
         float g = 182f / 255f;
