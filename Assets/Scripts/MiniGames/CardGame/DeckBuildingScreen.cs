3e2e8941cc850560be64c6379b55b46f9a80c425
Conor <34079159+ConorDennison@users.noreply.github.com>
2024-04-04 10:27:24 -0600

    Added an are you sure prompt on bad deck

--- Assets/Scripts/MiniGames/CardGame/DeckBuildingScreen.cs
+++ Assets/Scripts/MiniGames/CardGame/DeckBuildingScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
@@ -11,10 +12,14 @@ public class DeckBuildingScreen : MonoBehaviour
     public InventoryManager ActionInventoryManager;
     public GameObject StartServiceButton;
     public List<InventoryItemData> ActionInventory;
+    public GameObject AreYouSurePrompt;
+
+    public List<ItemData> MandatoryItems = new List<ItemData>();
 
     // Start is called before the first frame update
     void Start()
     {
+        AreYouSurePrompt.SetActive(false);
         GameEventManager.instance.OnCloseMenu += OnCloseInventory;
         PlayerInventoryManager.InitializeInventoryManager(GameManager.Instance.PlayerState.Inventory);
         DeckInventoryManager.InitializeInventoryManager(GameManager.Instance.PlayerState.Deck);
@@ -53,6 +58,30 @@ public class DeckBuildingScreen : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    public void TryStartService()
+    {
+        if ((GameManager.Instance.GetGameScene() == EGameScene.InnInterior) || (GameManager.Instance.GetGameScene() == EGameScene.Tutorial))
+        {
+            bool Success = true;
+            foreach(ItemData i in MandatoryItems)
+            {
+                if (DeckInventoryManager.InventoryDataRef.Find(r => r.Data == i) == null)
+                {
+                    Success = false;
+                };
+            }
+
+            if(Success)
+            {
+                StartService();
+            }
+            else
+            {
+                AreYouSurePrompt.SetActive(true);
+            }
+        }
+    }
+
     public void StartService()
     {
         if((GameManager.Instance.GetGameScene() == EGameScene.InnInterior) || (GameManager.Instance.GetGameScene() == EGameScene.Tutorial))
@@ -64,5 +93,10 @@ public class DeckBuildingScreen : MonoBehaviour
         {
 
         }
+    }
+
+    public void CloseAreYouSurePrompt()
+    {
+        AreYouSurePrompt.SetActive(false);
     }
 }
