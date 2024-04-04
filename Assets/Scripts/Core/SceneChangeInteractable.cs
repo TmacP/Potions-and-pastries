4860795f4bd40d4425d3462e5a93b0500a984a0e
AndyMandyCandy <andyanan124@gmail.com>
2024-04-03 20:05:58 -0600

    adjusted "SceneChangeInteractable" to accept the transition prefab (no visible gameplay change yet)

--- Assets/Scripts/Core/SceneChangeInteractable.cs
+++ Assets/Scripts/Core/SceneChangeInteractable.cs
@@ -7,18 +7,35 @@ public class SceneChangeInteractable : MonoBehaviour, IInteractable
 
     [SerializeField] private string Prompt;
     [SerializeField] private EGameScene NewLocationScene;
+    public GameObject transitionObject;
     //**************** IInteractable
     public string InteractionPrompt => Prompt;
 
     public EInteractionResult TryInteract(InteractorBehavoir InInteractor, List<InventoryItemData> InteractionItem = null)
     {
-        SFX.PlayDoorOpen();
-        GameManager.Instance.ChangeGameScene(NewLocationScene);
-        if (NewLocationScene == EGameScene.InnInterior) // if we are going to the alpha interior, we increment the day
+        if (transitionObject == null)
         {
-            GameManager.Instance.GameDay++;
+            SFX.PlayDoorOpen();
+            GameManager.Instance.ChangeGameScene(NewLocationScene);
+            if (NewLocationScene == EGameScene.InnInterior) // if we are going to the alpha interior, we increment the day
+            {
+                GameManager.Instance.GameDay++;
+            }
+            return EInteractionResult.Success;
         }
-        return EInteractionResult.Success;
+        else
+        {
+            transitionObject.GetComponent<TransitionController>().ExitSceneTransition();
+            GameManager.Instance.ChangeGameScene(NewLocationScene);
+            if (NewLocationScene == EGameScene.InnInterior) // if we are going to the alpha interior, we increment the day
+            {
+                GameManager.Instance.GameDay++;
+            }
+            return EInteractionResult.Success;
+        }
+        
     }
-//********* End of IInteractable
+    //********* End of IInteractable
+
+
 }
